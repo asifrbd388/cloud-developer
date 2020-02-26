369226ef0e970f400a59f0ff217a4c9c06f421e3
Hasan Musfiqur Rahman <asif@Hasans-MacBook-Pro-2.local>
2020-02-26 20:29:43 +0600

    Adding code for the project. Added the end point to filter an end point from a public url

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -30,6 +30,49 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+
+ //function to validate the url 
+function validateURL(pURL: string) {
+  var regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
+  var url = new RegExp(regexQuery,"i");
+  return url.test(pURL);
+}
+
+  app.get( "/filteredimage", async ( req, res ) => {
+
+    // 1. validate the image_url query
+
+    var image_url = req.query.image_url;
+    var is_image_url_valid = validateURL(image_url);
+
+    if(is_image_url_valid){
+      // 2. call filterImageFromURL(image_url) to filter the image
+      var image_path = await filterImageFromURL(image_url);
+
+      var options = {
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+      };
+      // 3. send the resulting file in the response
+      res.sendFile(image_path, options, function (err) {
+        if (err) {
+          res.status(400).send('Image could not be accessed')
+        } else {
+          // 4. deletes any files on the server on finish of the response
+          deleteLocalFiles([image_path]);
+        }
+      });
+
+    }
+    else {
+      res.status(404).send('URL for the image was not found')
+    }
+  });
+
+
   
   // Root Endpoint
   // Displays a simple message to the user
